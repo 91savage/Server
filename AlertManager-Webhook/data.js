35a1ed7bee56d110a54de04660e4c99827b714ca
91savage <ekdma1403@gmail.com>
2022-11-14 16:35:49 +0900

    통신 테스트

--- AlertManager-Webhook/data.js
+++ AlertManager-Webhook/data.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
     receiver: 'webhook',
     status: 'firing',
     alerts: [
--- AlertManager-Webhook/routes/index.js
+++ AlertManager-Webhook/routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+var nodemailer = require('nodemailer');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function (req, res, next) {
+    var data = require('../data');
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: 'ekdma1403@gmail.com',
+            pass: 'xqmccweihrjzbdga',
+        },
+    });
+    data.alerts.forEach(alert => {
+        const labels = alert.labels;
+        var qs = '?';
+        Object.keys(labels).forEach(k => {
+            qs += `${k}=${labels[k]}&`;
+        });
+        transporter.sendMail(
+            {
+                from: 'Alertmanager <ekdma1403@gmail.com>',
+                to: 'ekdma1403@gmail.com',
+                subject: alert.annotations.summary,
+                html: `<div>${alert.annotations.description}</div>` + `<a href=""> Turn off</a>`,
+            },
+            e => {
+                if (e) res.status(500).json({ result: 'fail' });
+                else res.status(200).json({ result: 'success' });
+            }
+        );
+    });
 });
 
 module.exports = router;
